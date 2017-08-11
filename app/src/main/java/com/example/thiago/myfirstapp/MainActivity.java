3ae24cb0806a11b79da50680de7253a62462fd01
thiagocesarm <thiago_cesarm@hotmail.com>
2017-08-11 18:04:42 -0300

    Update

--- app/src/main/java/com/example/thiago/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/thiago/myfirstapp/MainActivity.java
@@ -8,6 +8,8 @@ import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String EXTRA_MESSAGE = "abc";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
